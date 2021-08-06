b74719e9fcd20b5e8c2ca356ae838700bb832a12
Maureen-1998DEV <maureen.ougo@student.moringaschool.com>
2021-08-06 12:12:19 +0300

    modified sigup

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function loginValidate(){
-  var inputEmail=$("#inputEmail").val() ;
+  var inputEmail=$("#inputEmail1").val() ;
   var passwordLogIn = $("#passwordLogIn").val() ;
   if(inputEmail!=""&& passwordLogIn!=""&& passwordLogIn.length>=5){
       window.location.href="main.html";
